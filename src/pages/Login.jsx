3b8bd389539a4a86766c60e063acda6a0bbb1851
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-26 19:46:19 -0300

    add style to the login page :clinking_glasses:

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,13 +22,18 @@ export function Login({ handleLogin }) {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen top-0 left-0 bg-zinc-950 fixed z-10 bg-opacity-50">
-      <div className="bg-green- flex flex-col gap-2 text-zinc-950">
-        <label htmlFor="user">Usuário:</label>
-        <input className="input input-bordered w-full max-w-xs" type="text" name="user" id="user" value={user} onChange={handleSetUser} />
-        <label htmlFor="user">Senha:</label>
-        <input className="input input-bordered w-full max-w-xs" type="text" name="password" id="password" value={psswd} onChange={handleSetPsswd} />
-        <button type="submit" onClick={onHandleLogin}>Entrar</button>
+    <div className="flex items-center justify-center h-screen w-screen top-0 left-0 bg-zinc-950 fixed z-10 bg-opacity-30 backdrop-blur-sm">
+      <div className="bg-primary rounded-box shadow-xl p-8 flex flex-col items-center gap-5 text-base-100">
+        <h2>Seja bem vindo!</h2>
+        <label htmlFor="user">
+          Usuário:
+          <input className="input input-bordered w-full max-w-xs text-zinc-950 px-2.5" type="text" name="user" id="user" value={user} onChange={handleSetUser} />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input className="input input-bordered w-full max-w-xs text-zinc-950" type="password" name="password" id="password" value={psswd} onChange={handleSetPsswd} />
+        </label>
+        <button className='btn btn-ghost text-base-100 active:text-accent w-full' type="submit" onClick={onHandleLogin}>Entrar</button>
       </div>
     </div>
   )
